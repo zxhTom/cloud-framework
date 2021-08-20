4c2609ed90184b2d70f823b327160fe5363aca68
zxhtom <870775401@qq.com>
2021-08-20 17:23:23 +0800

    更新架构中arctileId规范 引入web模块，分离springboot形式 TODO： 1、依赖目前在root中，后续计划依赖单独抽离靠pom方式import 2、root中仅做架构配置方向功能

---
+++ org-framework-web/src/main/java/com/github/zxhtom/web/WebApplication.java
@@ -0,0 +1,17 @@
+package com.github.zxhtom.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张新华
+ * @version V1.0
+ * @Package com.github.zxhtom.demo
+ * @date 2021/8/20 14:35
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
