b02eefbd0b800d6fd6a6777baa409f10823d7063
zxhtom <870775401@qq.com>
2022-09-16 08:47:11 +0800

    feat($demo): 新增各种测试用例
    1、exception新增多接口bean注册
    2、新增poi导出接口设计文档
    3、新增事物测试
    4、新增全局配置属性类分项

---
+++ org-framework-basics/org-framework-demos/org-framework-demo-exception/src/main/java/com/github/zxhtom/demo/exception/controller/RequestController.java
@@ -0,0 +1,29 @@
+package com.github.zxhtom.demo.exception.controller;
+
+import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 用于模拟测试request异步开发
+ * @author zxhtom
+ * 2022/9/14
+ */
+@RestController
+@RequestMapping(value = "/request")
+public class RequestController {
+
+    @RequestMapping(value = "/first3" , method = RequestMethod.GET)
+    public void first(HttpServletRequest request) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("name = " + request.getParameter("name"));
+    }
+}
